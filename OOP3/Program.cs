3e8deec716c42d7fd8e8e8960f168d898314e2b3
yusuf <yusuferaslan@hotmail.com>
2023-05-19 23:26:46 +0300

    inheritance(miras),interface,logger service,method injection

---
+++ OOP3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtıyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(konutKrediManager);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager };
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
